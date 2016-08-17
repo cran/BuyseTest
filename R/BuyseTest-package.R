466ca396cf9d54ccf3a294e7ab4a936539746d94
brice ozenne <brice.ozenne@orange.fr>
2016-08-17 10:40:20 +0000

    version 1.0

---
+++ R/BuyseTest-package.R
@@ -0,0 +1,18 @@
+#' @docType package
+#' @name BuyseTest
+#' @useDynLib BuyseTest
+#' @import data.table
+#' @importFrom lava categorical coxExponential.lvm distribution eventTime lvm sim
+#' @import methods
+#' @importFrom parallel detectCores
+#' @import Rcpp
+#' @import snowfall
+#' @importFrom stats as.formula na.omit rbinom
+#' @importFrom stats4 summary
+#' @importFrom survival survfit Surv
+#' @importFrom tcltk tkProgressBar setTkProgressBar
+#' @importFrom utils tail
+NULL
+
+
+#### #' @importFrom snowfall sfClusterEval sfExport sfInit sfLibrary sfStop
